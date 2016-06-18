3363abd6d82edcf6e014027e10f14e71ecd4b796
aw4y <aw4y@rehacktive.net>
2016-06-18 11:36:59 +0100

    docs update + move class

--- waspdb/src/main/java/net/rehacktive/waspdb/internals/collision/WaspDataPage.java
+++ waspdb/src/main/java/net/rehacktive/waspdb/internals/collision/WaspDataPage.java
@@ -1,4 +1,4 @@
-package net.rehacktive.waspdb.internals.collision.exceptions;
+package net.rehacktive.waspdb.internals.collision;
 
 /**
  * Created by stefano on 14/06/2016.
@@ -8,6 +8,9 @@ public class WaspDataPage {
 
     private byte[] iv;
     private byte[] data;
+
+    public WaspDataPage() {
+    }
 
     public byte[] getIv() {
         return iv;
